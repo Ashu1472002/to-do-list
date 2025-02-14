79293227d98c74239c8fc960f3335a03b471db69
Ashwini Pawar <ashu1472002@gmail.com>
2025-02-14 13:22:44 +0530

    final changes

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,10 +1,52 @@
+import { useEffect, useState } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 function App() {  
+
+  const [todos, setTodos] = useState([])
+  const [todoValue, setTodoValue] = useState('')
+
+  function persistData(newList) {
+    localStorage.setItem('todos',JSON.stringify({todos:newList}))
+  }
+
+  function handleAddTodos(newTodo){
+    const newTodoList = [...todos, newTodo]
+    persistData(newList)
+    setTodos(newTodoList)
+  }
+
+  function handleDeleteTodo(index){
+    const newTodoList = todos.filter((todo, todoIndex)=>{
+      return todoIndex !== index
+    })
+    persistData(newList)
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(index){
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
+  }
+
+  useEffect(()=> {
+    if(!localStorage){
+      return
+    }
+    let localtodos= localStorage.getItem('todos')
+    if(!localtodos){
+      return
+    }
+
+    localtodos = JSON.parse(localtodos).todos
+    setTodos(localtodos)
+  },[])
+
   return (
     <>
-      <TodoInput />
-      <TodoList />
+      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos}/>
+      <TodoList handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} todos={todos}/>
     </>
   )
 }
--- reactjs-todolist/src/components/TodoInput.jsx
+++ reactjs-todolist/src/components/TodoInput.jsx
@@ -1,10 +1,20 @@
-export default function TodoInput(){
+import { useState } from "react"
+
+export default function TodoInput(props){
+    const { handleAddTodos, todoValue, setTodoValue} = props
+    
     return(
-        <div>
-            <header>
-                <input placeholder="Enter Todo content..."/>
-                <button>Add</button>
-            </header>
-        </div>
+            <div>
+                <h1>Let's make a ToDo</h1>
+                <header>
+                    <input value={todoValue} onChange={(e)=>{
+                        setTodoValue(e.target.value)
+                    }} placeholder="Enter Todo content..."/>
+                    <button onClick={()=>{
+                        handleAddTodos(todoValue)
+                        setTodoValue('')
+                    }}>Add</button>
+                </header>
+            </div>
     )
 }
